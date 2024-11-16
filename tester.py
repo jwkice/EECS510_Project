6f0585ee5c0478fbcc7351850f986748b26bd2f7
Jake Kice <j118k994@ku.edu>
2024-11-16 17:16:43 -0600

    Improved testing ability with both file sorced and user entered strings, added test file

--- tester.py
+++ tester.py
@@ -2,15 +2,30 @@
 
 from automaton import Automaton
 
-def main():
-    input_string = '1+1'
-    print(input_string)
-
+def test_string(string):
+    print(string)
     machine = Automaton()
-
-    for i in input_string:
+    for i in string:
         print(machine.get_current_state() + ", " + i + " -> ", end='')
         print(machine.transition(i))
     print(machine.check())
+    print('')
+
+def main():
+    mode = input("Select mode - (f)ile or (u)ser:")
+    if mode == 'f':
+        input_file = open(input("Enter file name: "), 'r')
+        for line in input_file:
+            test_string(line.strip())
+    elif mode == 'u':
+        continue_loop = True
+        while continue_loop:
+            input_string = input("Enter test string (to quit, enter 'quit'):")
+            if input_string == 'quit':
+                continue_loop = False
+            else:
+                test_string(input_string)
+    else:
+        print("Invalid mode selection.")
 
 main()
