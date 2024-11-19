b0ade855ae21633cb652b4f870cdf959b84f82f5
Jake Kice <j118k994@ku.edu>
2024-11-19 08:01:26 -0600

    Removed get_start_state function, added description comments

--- automaton.py
+++ automaton.py
@@ -1,4 +1,26 @@
 #Arithmetic Language Automaton
+'''
+Name: EECS 510 Formal Language Project Automaton - Arithmetic Language
+Author: Jacob Kice
+Date Created: 11/16/2024
+Date Modified: 11/19/2024
+Function __init__ defines the state details: a list of all states, the start state,
+    the jail state, and the accepting states. It also initializes the stack and
+    defines the three character sets: numeral, alphabetic, and operator.
+Function get_start_state returns the start state of the machine.
+Function get_jail_state returns the jail state of the machine.
+Function transition defines the transitions of the machine. It consists of a two
+    level nested if-statement. The outer level identifies the current state, as
+    passed by the caller. The inner level identifies the input character, also
+    passed by the caller. Each case of the outer level represents the output
+    transitions for its state in the machine, by defining the transitions from that
+    state for each allowed input character, as well as handling the stack maintainance as
+    needed. The nested if-statements also define jail state transitions for the
+    input characters that do not have a specifically defined transition from the current
+    state, using the else case on the inner if-statement.
+Function check determines if the string is accepted or rejected by the machine by checking
+    if the provided state is an accepting state and the stack is empty.
+'''
 
 class Automaton:
     def __init__(self):
@@ -7,13 +29,9 @@ class Automaton:
         self._jail_state = 'j'
         self._accepting_states = ['q1','q5']
         self._stack = []
-        self._current_state = self._start_state
         self._nums = ['0','1','2','3','4','5','6','7','8','9']
         self._alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         self._operators = ['+','-','*','/','%','^']
-
-    def get_current_state(self):
-        return self._current_state
     
     def get_start_state(self):
         return self._start_state
